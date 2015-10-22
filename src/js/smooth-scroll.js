f9946491ef7eae3ac75d27758c60bc1256ba6b8d
jaronbarends <jaron@jaron.nl>
2015-10-22 16:24:09 +0200

    removed resources

---
+++ src/js/smooth-scroll.js
@@ -0,0 +1,43 @@
+;(function($) {
+
+	'use strict';
+
+	// define semi-global variables (vars that are "global" in this file's scope) and prefix them
+	// with sg so we can easily distinguish them from "normal" vars
+	
+	var init = function(){
+		$('a[href*="#"]:not([href="#"])').click(function(event) {
+			if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+				//Filter target element
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+				if (target.length) {
+					//Prevent page load
+					event.preventDefault();
+					
+					//Calculate distance between target offset and current scroll offset, for animation timing (distance *  0.6 = time in ms).
+					var scrollTimer = (Math.abs(($(window).scrollTop()) - target.offset().top) * 0.6);
+					
+					//Stop running animations to prevent stacking, start a new scrolling animation.
+					$('html,body').stop().animate(
+						{scrollTop: target.offset().top}, 
+						scrollTimer
+					);
+					
+					//Prevent page load (Firefox)
+					return false;
+				}
+			}
+		});
+
+		//Cancel animation after manual scrolling/interaction
+		$('html,body').on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+			$('html,body').stop();
+  		});
+
+	};
+
+	$(document).ready(init);
+
+})(jQuery);
--- src/js/zij-hun-hen.js
+++ src/js/zij-hun-hen.js
@@ -21,6 +21,25 @@
 			$('body').toggleClass('disclosed');
 		});
 	};
+
+
+	/**
+	* initialize smooth scrolling
+	* @returns {undefined}
+	*/
+	var initSmoothLinks = function() {
+		$('.more').on('click', function(e) {
+		console.log('klik');
+			e.preventDefault();
+			var $link = $(e.currentTarget),
+				href = $link.attr('href'),
+				$dest = $(href),
+				destTop = $dest.scrollTop();
+
+			$('body').scrollTop(destTop);
+		});
+	};
+	
 	
 
 
@@ -31,9 +50,40 @@
 	*/
 	var init = function() {
 		initDiscloseLink();
+		// initSmoothLinks();
 	};
 
 
 	$(document).ready(init);
 
 })(jQuery);
+
+
+/*
+zij (persoonlijk voornaamwoord)
+1
+derde persoon vrouwelijk enkelvoud: zij is rijk
+2
+derde persoon meervoud voor alle geslachten: zij zijn rijk
+- See more at: http://www.vandale.nl/opzoeken?pattern=zij&lang=nn#.Vhffxfntmko
+
+
+
+hen (persoonlijk voornaamwoord; 4e nvl mv bij zij) zie 1hun
+
+2hen (de; v; meervoud: hennen; verkleinwoord: hennetje)
+1
+vrouwelijk hoen
+- See more at: http://www.vandale.nl/opzoeken?pattern=hen&lang=nn#.Vhflsfntmko
+
+
+
+hun (persoonlijk voornaamwoord; in geschreven taal 3e, in spreektaal 3e en 4e nvl van de 3e pers mv)
+1
+ik heb het hun gegeven
+
+2hun (bezittelijk voornaamwoord)
+1
+van 1hen: dat is hun huis
+- See more at: http://www.vandale.nl/opzoeken?pattern=hun&lang=nn#.VhfmR_ntmko
+*/
