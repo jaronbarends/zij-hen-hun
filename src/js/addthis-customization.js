2ce65829ef40a2a9c0cde6e272518c59239a2186
jaronbarends <jaron@jaron.nl>
2016-04-03 17:49:31 +0200

    removed addthis tracking code

--- src/js/addthis-customization.js
+++ src/js/addthis-customization.js
@@ -1,9 +1,12 @@
-var addthis_share = addthis_share || {}
-addthis_share = {
-	passthrough : {
-		twitter: {
-			via: "jaronbarends",
-			text: "Was het nou zij, hen of hun? Lekker (on)duidelijk uitgelegd."
-		}
+var addthis_share = addthis_share || {},
+	addthis_config = addthis_config || {};
+
+addthis_config.data_track_addressbar = false;
+addthis_config.data_track_clickback = false;
+
+addthis_share.passthrough = {
+	twitter: {
+		via: "jaronbarends",
+		text: "Was het nou zij, hen of hun? Lekker (on)duidelijk uitgelegd."
 	}
 };
